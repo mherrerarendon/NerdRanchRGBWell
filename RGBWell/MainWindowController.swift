f8a9c94b7899ac642180f525a19c876d578ba86e
Marco Herrera <marcoherrera@Marcos-Mac-Pro.local>
2016-02-16 13:26:28 -0700

    first commit on Xcode

---
+++ RGBWell/MainWindowController.swift
@@ -0,0 +1,17 @@
+//
+//  MainWindowController.swift
+//  RGBWell
+//
+//  Created by Marco Herrera on 2/16/16.
+//  Copyright (c) 2016 Marco Herrera. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController
+{
+    override var windowNibName: String?
+    {
+        return "MainWindowController"
+    }
+}
